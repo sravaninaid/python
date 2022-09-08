6e31d5f8a2672b3255dd694644d46ab7861a4d51
sravaninaid <112851292+sravaninaid@users.noreply.github.com>
2022-09-08 10:30:08 +0530

    Add files via upload

---
+++ grade and average program.py
@@ -0,0 +1,17 @@
+g1 = float(input("enter the stub 1 marks"))
+g2 = float(input("enter the stub 1 marks"))
+g3 = float(input("enter the stub 1 marks"))
+average = float(g1+g2+g3)/3
+print(average)
+if(average>=90):
+    print("grade is A")
+elif(average>=80):
+    print("grade is B")
+elif(average>=70):
+    print("grade is C")
+elif(average>=60):
+    print("grade is C")
+elif(average>=50):
+    print("grade is C")
+else:
+    print("fail")
